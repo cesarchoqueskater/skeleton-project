9ef947452f0088b8997654585d0953ae16ba1c71
cchoqueh <cesarchoqueskater@gmail.com>
2021-10-08 00:47:21 -0500

    Fix en logica de loader

--- js/content.js
+++ js/content.js
@@ -1,37 +1,56 @@
 import { createDOM } from './utils/dom.js'
 import { cutSpace } from './utils/format-data.js'
 
-export function cardContentTemplate({ index, id, title, author }) {
+export function cardContentTemplate(index) {
     return `
-            <div class="card-content" id=${index}>
-                <div class="card-image content-background skeleton" id="card-image">
-                    <img src="https://picsum.photos/id/${id}/200/300" alt=""/> 
-                </div>
+            <div class="card-content" id="content-${index}">
+                <div class="card-image content-background skeleton" id="card-image"></div>
                 <div class="card-description">
-                    <div class="card-description-subject subject-medium skeleton" id="subject-medium">${title}</div>
+                    <div class="card-description-subject subject-medium skeleton" id="subject-medium"></div>
                     <div class="card-description-subject subject-small skeleton" id="subject-small"></div>
                     <div class="card-description-subject subject-large skeleton" id="subject-large"></div>
-                    <div class="card-writer">
+                    <div class="card-writer" id="content-writer-${index}">
                         <div class="writer-information">
                             <div class="writer-image skeleton" id="writer-image">
-                                <img src="https://picsum.photos/id/${id}/200/300" alt=""/>
                             </div>
-                            <div class="writer-name skeleton" id="writer-name">${author}</div>
+                            <div class="writer-name skeleton" id="writer-name"></div>
                         </div>
-                        <div class="writer-date skeleton" id="writer-date">8 min read</div>
+                        <div class="writer-date skeleton" id="writer-date"></div>
                     </div>
                 </div>
             </div>
         `
 }
 
-export function createCardContent(data, index) {
-    const values = {
-            index,
-            id: data.id,
-            title: data.author,
-            author: cutSpace(data.author),
-        }
-        // debugger
-    return createDOM(cardContentTemplate(values, index))
+export function setImage(value, index) {
+    const $cardImage = document.querySelector(`#content-${index} .card-image`)
+    $cardImage.innerHTML = `<img src="https://picsum.photos/id/${value.id}/200/300" alt="${value.author}"/>`
+}
+
+export function setTitle(value, index) {
+    const $setTitle = document.querySelector(`#content-${index} .card-description #subject-medium`)
+    $setTitle.textContent = `${value.author}`
+}
+
+export function setImageAuthor(value, index) {
+    const $setImageAuthor = document.querySelector(`#content-writer-${index} .writer-information .writer-image`)
+    $setImageAuthor.innerHTML = `<img src="https://picsum.photos/id/${value.id}/200/300" alt="${value.author}"/>`
+}
+
+export function setAuthor(value, index) {
+    const $setAuthor = document.querySelector(`#content-writer-${index} .writer-information  #writer-name`)
+    $setAuthor.textContent = cutSpace(`${value.author}`)
+}
+
+export function setRandomDate(index) {
+    const $setAsetRandomDateuthor = document.querySelector(`#content-writer-${index} #writer-date`)
+    const valueMax = 60
+    const valueMin = 1
+    const valueRandom = Math.floor(Math.random() * (valueMax - valueMin + 1) + valueMin);
+    $setAsetRandomDateuthor.innerHTML = `<p>${valueRandom} min read</p>`
+}
+
+
+export function createCardContent(index) {
+    return createDOM(cardContentTemplate(index))
 }
--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 import { getImages } from './api.js'
-import { createCardContent } from './content.js'
-import { createDOM } from './utils/dom.js'
+import { createCardContent, setImage, setTitle, setImageAuthor, setAuthor, setRandomDate } from './content.js'
 
 function createContainerPanel(data, index) {
     const $panel = createCardContent(data, index)
-        // if (data > 0) {
-        //     $panel.hidden = true
-        // }
-        // debugger
     return $panel
 }
 
 function loader(isLoading) {
     if (isLoading) {
+        const $cardTitleHeader = document.querySelector('#cardTitleHeader')
+        $cardTitleHeader.classList.remove('skeleton')
+        cardTitleHeader.style.inlineSize = 'auto'
+        const $cardImageHeader = document.querySelector('#cardImageHeader')
+        $cardImageHeader.classList.remove('skeleton')
+
+
         const $cardImage = document.querySelectorAll('#card-image')
         const $subjectMedium = document.querySelectorAll('#subject-medium')
         const $subjectSmall = document.querySelectorAll('#subject-small')
         const $subjectLarge = document.querySelectorAll('#subject-large')
         const $writerName = document.querySelectorAll('#writer-name')
         const $writerDate = document.querySelectorAll('#writer-date')
-
         $cardImage.forEach((cardImage) => {
             cardImage.classList.remove('skeleton')
         })
@@ -43,18 +44,31 @@ function loader(isLoading) {
         })
     }
 }
+
+function setHeader() {
+    const $cardTitleHeader = document.querySelector('#cardTitleHeader')
+    const $cardImageHeader = document.querySelector('#cardImageHeader')
+    $cardImageHeader.innerHTML = `<img src="./images/logo-no-te-rindas.png" alt=""/>`
+    $cardTitleHeader.innerHTML = `<a>LEONIDAS ESTEBAN.COM</a>`
+}
 export async function showImages() {
     const $container = document.querySelector('.content')
     loader(false)
     const { data, isError } = await getImages()
 
     data.forEach((value, index) => {
-        const $panel = createContainerPanel(value, index)
+        const $panel = createContainerPanel(index)
         $container.append($panel)
+        setTimeout(() => {
+            setHeader()
+            setImage(value, index)
+            setTitle(value, index)
+            setImageAuthor(value, index)
+            setAuthor(value, index)
+            setRandomDate(index)
+            loader(true)
+        }, 5000)
     });
-    setTimeout(() => {
-        loader(true)
-    }, 1000)
 }
 
 showImages()
